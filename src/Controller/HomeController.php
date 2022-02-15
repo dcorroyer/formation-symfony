364a97296962d5ad0675daec7108e6712a02f1c0
dcorroyer <dylan.corroyer@wanadoo.fr>
2022-02-15 23:06:38 +0100

    doctrine init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+//use App\Entity\Product;
+//use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(): Response
+    public function homepage(
+//        EntityManagerInterface $em
+    ): Response
     {
+//        $productRepository = $em->getRepository(Product::class);
+//        $product           = $productRepository->find(3);
+//
+//        $product->setPrice(13000);
+//        $em->flush();
+
+//        $product = new Product();
+//
+//        $product
+//            ->setName('Souris gaming')
+//            ->setPrice(12000)
+//            ->setSlug('souris-gaming')
+//        ;
+//
+//        $em->persist($product);
+//        $em->flush();
+
+
         return $this->render('home.html.twig');
     }
 }
